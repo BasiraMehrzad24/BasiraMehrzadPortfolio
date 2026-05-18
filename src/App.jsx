a37085a1e6944776ba445a5efa1a4eb1b1ea4e23
BasiraMehrzad24 <basiramehrzad25@gmail.com>
2026-05-18 10:35:08 +0430

    add react vite proejct

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='text-5xl text-blue-400 text-center'>Basira Mehrzad</h1>
+      
+    </>
+  )
+}
+
+export default App
